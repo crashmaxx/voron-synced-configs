5715ec8cb095461c0c54ab40ed95a4c8b1d6471f
Andrew Stowell <crashmaxx@gmail.com>
2021-12-13 11:33:15 -0500

    Autocommit from 2021-12-13 11:33:15
    Klipper on commit: b99b45e
    
    Moonraker on commit: 3f7cc53
    
    Mainsail version: v2.1.0-beta4

--- scripts/git-backup.sh
+++ scripts/git-backup.sh
@@ -51,15 +51,47 @@ grab_version(){
 
 push_config(){
   cd $config_folder
-  git pull
-  git add .
-  current_date=$(date +"%Y-%m-%d %T")
-  git commit -m "Autocommit from $current_date" -m "$m1" -m "$m2" -m "$m3" -m "$m4"
-  git remote update
-  tag=$(git describe --tags --abbrev=0)
-  n_tag=$(./scripts/next-tag.sh $tag build)
-  git tag -a $n_tag -m "$HOSTNAME"
-  git push --follow-tags
+  if ! git fetch --all --quiet
+  then
+    if ! git diff --quiet
+    then
+      echo "Dirty, changes will be stashed."
+      git stash
+    fi
+    git pull
+  else
+    if ! git diff --quiet
+    then
+      echo "Clean, commiting changes."
+      git add .
+      current_date=$(date +"%Y-%m-%d %T")
+      git commit -m "Autocommit from $current_date" -m "$m1" -m "$m2" -m "$m3" -m "$m4"
+      next_tag
+      git tag -a $n_tag -m "$HOSTNAME"
+      git push --follow-tags
+    else
+      echo "No changes to commit. Already up to date."
+    fi
+  fi
+}
+
+next_tag(){
+  version=$(git describe --tags --abbrev=0)
+  major=0
+  minor=0
+  build=0
+  n_tag="v0.0.0"
+
+  # break down the version number into it's components
+  regex="v([0-9]+).([0-9]+).([0-9]+)"
+  if [[ $version =~ $regex ]]; then
+    major="${BASH_REMATCH[1]}"
+    minor="${BASH_REMATCH[2]}"
+    build="${BASH_REMATCH[3]}"
+  fi
+
+  build=$((build + 1))
+  n_tag="v${major}.${minor}.${build}"
 }
 
 grab_version
